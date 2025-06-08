a998c38cd71187917ef08db1d798df2478dd8bc1
lucap <lucap@DESKTOP-GKGGQJ6>
2025-06-08 18:39:05 +0200

    Aggiungere i file di progetto.

---
+++ it.lucaporfiri.appweb.core.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace it.lucaporfiri.appweb.core.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
