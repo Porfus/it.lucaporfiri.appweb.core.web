4896623acb249ca27bb7d354b1a6294b1e911ee0
lucap <lucaporfiri@gmail.com>
2026-01-09 21:48:44 +0100

    aggiunti stili e markup html della dashboard atleta area

---
+++ it.lucaporfiri.appweb.core.web/Controllers/AtletaArea.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace it.lucaporfiri.appweb.core.web.Controllers
+{
+    public class AtletaArea : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
